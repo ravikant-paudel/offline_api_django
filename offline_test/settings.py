acf7510d272f522723bfb61092d12a184e69ebd5
Ravikant Paudel <developer.paudel@gmail.com>
2023-09-06 09:34:10 +0545

    Add allow host in setting

--- offline_test/settings.py
+++ offline_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hx*jn775n207=ql%p%qc^-bm-s1rzhj0=9x(gaxlwe!)r6#@h*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.52.26']
 
 
 # Application definition
